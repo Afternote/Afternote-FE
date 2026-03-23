e073f9023a0c7e16f8f59f1d861b91582488c5e2
Sadturtleman <rugsn12345@gmail.com>
2026-03-23 18:47:21 +0900

    feat: 텍스트 스타일 구성

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,40 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("afternote.android.library.compose")
 }
 
 android {
-    namespace = "com.afternote.ui"
-    compileSdk {
-        version = release(36)
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+    namespace = "com.afternote.core.ui"
+    resourcePrefix = "core_ui_"
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ feature/mindrecord/presentation/src/main/kotlin/com/afternote/feature/mindrecord/presentation/screen/sender/HomeScreen.kt
@@ -0,0 +1,35 @@
+package com.afternote.feature.mindrecord.presentation.screen.sender
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.afternote.core.ui.TopBar
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = { TopBar() }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues).padding(horizontal = 22.5.dp)) {
+            Text(
+                text = "나의 기록",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview(){
+    HomeScreen()
+}
