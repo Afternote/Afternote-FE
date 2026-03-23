74b138df96d928d036419194d0f6b665705c7b8d
Sadturtleman <rugsn12345@gmail.com>
2026-03-23 23:37:10 +0900

    feat: base color add

---
+++ core/ui/src/main/kotlin/com/afternote/core/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.afternote.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+
+val Black = Color(0xFF000000)
+
+val IconBk = Color(0xFF000000).copy(alpha = 0.6F)
+
+val Gray1 = Color(0xFFFAFAFA)
+
+val Gray2 = Color(0xFFEEEEEE)
+
+val Gray3 = Color(0xFFE0E0E0)
+
+val Gray4 = Color(0xFFBDBDBD)
+
+val Gray5 = Color(0xFF9E9E9E)
+
+val Gray6 = Color(0xFF757575)
+
+val Gray7 = Color(0xFF616161)
+
+val Gray8 = Color(0xFF424242)
+
+val Gray9 = Color(0xFF212121)
