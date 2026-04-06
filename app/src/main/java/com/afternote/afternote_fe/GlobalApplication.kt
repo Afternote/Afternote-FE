5d7ede2daea2732ec645b655c77d2f1e5f4a08a6
일혁 <dnfjddk2@gmail.com>
2026-04-07 02:27:39 +0900

    ImageApiService에 S3 파일 업로드 기능 추가 및 인터페이스 정의 변경
    - S3 Presigned URL을 사용하여 파일을 업로드하는 `uploadToS3` 메서드 추가
    - 추상 메서드가 추가됨에 따라 `fun interface`를 일반 `interface`로 변경

--- app/src/main/java/com/afternote/afternote_fe/GlobalApplication.kt
+++ app/src/main/java/com/afternote/afternote_fe/GlobalApplication.kt
@@ -1,12 +1,13 @@
 package com.afternote.afternote_fe
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-// TODO:KakaoSdk.init(this, "{네이티브_APP_KEY}")
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 }
