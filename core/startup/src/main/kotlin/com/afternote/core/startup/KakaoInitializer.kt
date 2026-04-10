40e6faa10a836b5dd50db1fbb38daf52f1d3a2f6
일혁 <dnfjddk2@gmail.com>
2026-04-10 20:15:35 +0900

    Kakao SDK 초기화 방식 및 BuildConfig 참조 관련 주석 보완
    - Kakao SDK 초기화를 Application이 아닌 App Startup에서 수행하는 이유 명확화
    - 라이브러리 모듈에서 app 모듈의 `BuildConfig`를 참조할 수 없는 제약 사항 및 `core:startup` 내 `BuildConfig` 생성 방식에 대한 설명 추가

--- core/startup/src/main/kotlin/com/afternote/core/startup/KakaoInitializer.kt
+++ core/startup/src/main/kotlin/com/afternote/core/startup/KakaoInitializer.kt
@@ -5,7 +5,10 @@ import androidx.startup.Initializer
 import com.kakao.sdk.common.KakaoSdk
 
 /**
- * 카카오 SDK 초기화. [GlobalApplication]이 아닌 App Startup으로 실행해 Application 본문을 비운다.
+ * 카카오 SDK 초기화. `Application`이 아닌 App Startup으로 실행한다.
+ *
+ * `app` 모듈의 [com.afternote.afternote_fe.BuildConfig]는 라이브러리에서 참조할 수 없으므로
+ * [BuildConfig]는 `core:startup`의 `build.gradle.kts`에서 동일 키로 생성한다.
  */
 class KakaoInitializer : Initializer<Unit> {
     override fun create(context: Context) {
