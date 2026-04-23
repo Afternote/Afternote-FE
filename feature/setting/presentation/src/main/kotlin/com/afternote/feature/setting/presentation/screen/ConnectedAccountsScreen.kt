c882ddd32d8205624675f5cf8a515405c95d36ba
kyungmin <minii7582@konkuk.ac.kr>
2026-04-23 14:17:38 +0900

    feat: 연결된 계정 화면 추가

---
+++ feature/setting/presentation/src/main/kotlin/com/afternote/feature/setting/presentation/screen/ConnectedAccountsScreen.kt
@@ -0,0 +1,15 @@
+package com.afternote.feature.setting.presentation.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ConnectedAccountScreen(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun ConnectedAccountScreenPrev() {
+    ConnectedAccountScreen()
+}
