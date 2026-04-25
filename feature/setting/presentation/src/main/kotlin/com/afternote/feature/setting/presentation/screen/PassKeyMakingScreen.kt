ffd4b1a974d25a875a08cc40c81b9507a3c86f14
kyungmin <minii7582@konkuk.ac.kr>
2026-04-25 10:03:09 +0900

    feat: 패스키 관리 추가

--- feature/setting/presentation/src/main/kotlin/com/afternote/feature/setting/presentation/screen/PassKeyMakingScreen.kt
+++ feature/setting/presentation/src/main/kotlin/com/afternote/feature/setting/presentation/screen/PassKeyMakingScreen.kt
@@ -1,12 +1,22 @@
 package com.afternote.feature.setting.presentation.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.afternote.core.ui.button.AfternoteButton
+import com.afternote.core.ui.button.AfternoteButtonType
 import com.afternote.core.ui.topbar.DetailTopBar
 import com.afternote.feature.setting.presentation.R
 
@@ -23,10 +33,38 @@ fun PassKeyMakingScreen(
             )
         },
     ) { innerPadding ->
-        Text(
-            text = stringResource(R.string.passkey_fingerprint_guide),
-            modifier = Modifier.padding(innerPadding),
-        )
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(137.dp))
+            Text(
+                text = stringResource(R.string.passkey_fingerprint_guide),
+                modifier = Modifier.padding(innerPadding),
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Image(
+                painterResource(R.drawable.ic_fingerprint),
+                "지문",
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+            AfternoteButton(
+                text = "지문 인증하기",
+                onClick = {},
+                type = AfternoteButtonType.Default,
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            AfternoteButton(
+                text = "비밀번호로 인증하기",
+                onClick = {},
+                type = AfternoteButtonType.Active,
+            )
+        }
     }
 }
 
