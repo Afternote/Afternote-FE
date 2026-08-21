386394a91ddcfc09ddfc029928b9123699697286
kyungmin <minii7582@konkuk.ac.kr>
2026-08-21 20:28:20 +0900

    test: configure Robolectric SDK for time letter view model

--- feature/timeletter/presentation/src/test/kotlin/com/afternote/feature/timeletter/presentation/viewmodel/TimeLetterWriteViewModelTest.kt
+++ feature/timeletter/presentation/src/test/kotlin/com/afternote/feature/timeletter/presentation/viewmodel/TimeLetterWriteViewModelTest.kt
@@ -38,9 +38,11 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class TimeLetterWriteViewModelTest {
     private val dispatcher = UnconfinedTestDispatcher()
 
